2d10158e2f4268aa346f4ab70d1bc1b6182d71d1
DiorHomme <chomunodisaa68@gmail.com>
2026-03-07 11:16:15 +0200

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar.component';
+import { Footer } from './app/shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, Footer],
   template: `
     <app-navbar></app-navbar>
     <router-outlet></router-outlet>
+    <app-footer></app-footer>
   `,
   styleUrl: './app.css'
 })
