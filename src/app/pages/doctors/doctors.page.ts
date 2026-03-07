72aa98e8ed18eeccb80b831567731d963dc7c623
DiorHomme <chomunodisaa68@gmail.com>
2026-03-07 11:18:57 +0200

    Create doctors.page.ts

---
+++ src/app/pages/doctors/doctors.page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctors-page',
+  standalone: true,
+  templateUrl: './doctors.page.html',
+  styleUrl: './doctors.page.css'
+})
+export class DoctorsPage {}
