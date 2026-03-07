4a6f5b7911757f7970947bc9916298dd6b7e59c0
DiorHomme <chomunodisaa68@gmail.com>
2026-03-07 11:20:22 +0200

    Update navbar.component.ts

--- src/app/shared/navbar.component.ts
+++ src/app/shared/navbar.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   templateUrl: './navbar.component.html',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, FormsModule],
   styles: []
 })
 export class NavbarComponent {
+
+  menuOpen = false;
+
+  isModalOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  openModal() {
+    console.log('Modal opened');
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  bookAppointment() {
+    alert('Appointment booked successfully!');
+    this.closeModal();
+  }
 }
