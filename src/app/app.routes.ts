9cf984eccb1d3973bfca82b9eb14dcbc7c963ede
DiorHomme <chomunodisaa68@gmail.com>
2026-03-07 11:16:11 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { AboutPage } from './pages/about/about.page';
+import { DoctorsPage } from './pages/doctors/doctors.page';
+import { ServicesPage } from './pages/services/services.page';
+import { ContactPage } from './pages/contact/contact.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about', component: AboutPage },
+  { path: 'doctors', component: DoctorsPage },
+  { path: 'services', component: ServicesPage },
+  { path: 'contact', component: ContactPage }
+];
