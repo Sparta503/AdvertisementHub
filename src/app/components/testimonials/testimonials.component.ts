0cc7a5bd39e51015439621214360189afa23a8fe
DiorHomme <chomunodisaa68@gmail.com>
2026-03-07 11:17:48 +0200

    Create testimonials.component.ts

---
+++ src/app/components/testimonials/testimonials.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testimonials',
+  standalone: true,
+  templateUrl: './testimonials.component.html',
+  styleUrl: './testimonials.component.css'
+})
+export class TestimonialsComponent {
+}
