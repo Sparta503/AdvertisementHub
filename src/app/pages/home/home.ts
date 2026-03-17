f722971279752bb71761fe14287a77cdce4eca01
DiorHomme <chomunodisaa68@gmail.com>
2026-03-17 17:14:34 +0200

    Update home.ts

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  AfterViewInit,
+  ElementRef,
+  QueryList,
+  ViewChildren
+} from '@angular/core';
+
 import { HeroComponent } from '../../components/hero/hero.component';
 
 @Component({
@@ -6,6 +13,35 @@ import { HeroComponent } from '../../components/hero/hero.component';
   standalone: true,
   imports: [HeroComponent],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home {}
+export class Home implements AfterViewInit {
+
+  @ViewChildren('card') cards!: QueryList<ElementRef>;
+
+  ngAfterViewInit(): void {
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+
+        entries.forEach(entry => {
+
+          if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+          }
+
+        });
+
+      },
+      {
+        threshold: 0.3
+      }
+    );
+
+    this.cards.forEach(card => {
+      observer.observe(card.nativeElement);
+    });
+
+  }
+
+}
